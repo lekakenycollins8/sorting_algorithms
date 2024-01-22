aca0e0b03d74dbf9e00216b4bed30639317ffd67
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-01-22 03:05:27 -0800

    added condition in partition function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -36,12 +36,18 @@ int partition(int *array, int low, int high, size_t size)
 		if (array[j] <= pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
-			print_array(array, size);
+			if (array[i] != array[j])
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
+	if (array[i + 1] != array[high])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
 	return (i + 1);
 }
 
